35b1e83fb73aebe71e70df513fac3a4132a9f810
Apurva <100802168+ApurvaG04@users.noreply.github.com>
2024-12-08 15:57:21 -0500

    Update Readings.js

--- frontend/src/Readings.js
+++ frontend/src/Readings.js
@@ -20,7 +20,7 @@ function Readings() {
     useEffect (() => {
         (async () => {
             setLoading(true);
-            let data = await fetch('http://127.0.0.1:5000/readings', {
+            let data = await fetch(process.env.REACT_APP_API_URL + '/readings', {
             'method': 'GET',
             headers: {
                 'Content-Type': 'application/json'
